b4a54f537c26a4b55516a9460534ad910a060848
Roni <ronisank21@hotmail.com>
2026-07-16 21:47:01 -0300

    fix-remoto origin

--- SCTec-Angular-mod-1/Modulo-1/Semana3/Aula3.js
+++ SCTec-Angular-mod-1/Modulo-1/Semana3/Aula3.js
@@ -1,10 +1,35 @@
+console.log()
+console.log('-------------Atividade 1---------------------')
+console.log()
+
 let dataEntrada = '2026-07-16';
 
 let arrayData = dataEntrada.split('-');
 console.log(arrayData)
 
-console.log('Data Formatada: ' +arrayData[2] +'/'+ arrayData[1] + '/' + arrayData[0]);
+console.log('Data Formatada: ' + arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0]);
 
 for (let valor of arrayData) {
     console.log(valor);
 }
+console.log()
+console.log('-------------Atividade 2---------------------')
+console.log()
+
+const dados = [123, 'texto', true, 'palavra', 99, 'JS'];
+
+for (const index of dados) {
+    if (typeof(index) == 'string') {
+        console.log(index)
+    }
+}
+
+console.log()
+console.log('-------------Atividade 3---------------------')
+console.log()
+
+const emails = ["joao@email.com", "ana@email.com", "lucas@email.com"];
+
+emails.forEach((email) => {
+    console.log('Enviando e-mail para ' + email)
+});
