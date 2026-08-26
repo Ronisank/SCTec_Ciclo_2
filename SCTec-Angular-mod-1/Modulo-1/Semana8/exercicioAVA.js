da19e7a845371dddea9e064bdd985433fb4d7f24
Roni <ronisank21@hotmail.com>
2026-08-25 22:10:28 -0300

    Aula semana8

---
+++ SCTec-Angular-mod-1/Modulo-1/Semana8/exercicioAVA.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector("#botao");
+const mensagem = document.querySelector("#mensagem");
+
+// botao.addEventListener("click", () => { mensagem.textContent = "Olá!"; });
+const campoNome = document.querySelector("#nome");
+
+// campoNome.addEventListener("click", () => { console.log("Tecla pressionada"); });
+
+// campoNome.addEventListener("keyboard", () => { console.log("Tecla pressionada"); });
+
+// campoNome.addEventListener("keydown", () => { console.log("Tecla pressionada"); });
